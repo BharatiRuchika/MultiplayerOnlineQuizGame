0cad843b4a039456bca0a272e8be1f846a8cb6e7
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-12-17 14:33:34 +0530

    first commit

--- index.js
+++ index.js
@@ -324,10 +324,11 @@ app.use(cookieParser());
 //     });
 //   })
 // }
+app.use(express.static(path.join(__dirname, 'client/build')))
 if(process.env.NODE_ENV==="production"){
   // app.use(express.static(path.join(__dirname,"Frontend_Kahoot-ma/build")));
   const path = require("path");
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  
   app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build'))
   // res.send("im run");
