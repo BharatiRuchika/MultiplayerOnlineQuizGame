0f9417cc83c909f6ecd9733e0e758ca9bff615d7
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-12-17 14:21:33 +0530

    first commit

--- index.js
+++ index.js
@@ -327,9 +327,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV==="production"){
   // app.use(express.static(path.join(__dirname,"Frontend_Kahoot-ma/build")));
   const path = require("path");
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
   app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build'))
+  res.sendFile(path.join(__dirname, '/client/build'))
   // res.send("im run");
 })
 }
