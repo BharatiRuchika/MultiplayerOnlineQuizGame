dc3d9ee928d1490db0c94455f8500116e9a9bdd8
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-12-17 14:06:40 +0530

    first commit

--- index.js
+++ index.js
@@ -297,8 +297,8 @@ socket.on("disconnect",function(){
 
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -374,7 +374,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  // res.render('error');
+  res.render('error');
   res.json({ error: err })
 });
 
